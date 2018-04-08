b922fc306be7796a7669b0e7e8e04941ef915ba7
yinguohang <yinguohang@gmail.com>
2018-04-07 20:05:27 -0700

    Add simple rules

--- data.py
+++ data.py
@@ -15,6 +15,8 @@ from dateutil import parser
 import numpy as np
 import pandas as pd
 import json
+from IPy import IP
+import requests
 
 class MacParser:
 	def __init__(self, filename):
@@ -27,11 +29,36 @@ class MacParser:
 		return self.d.get(mac, "")
 
 class RuleList:
+	ports = []
+	protocols = [] 
 	def __init__(self):
 		self.white_list = [] #[(ip, port, protocol), ...]
 		self.black_list = []
+	def getSingleScore(self, list_ip, list_port, list_protocol, ip, port, protocol):
+		score = 0
+		if list_ip != None and list_ip != ip:
+			return 0
+		if list_port != None and list_port != port and port not in RuleList.ports:
+			return 0
+		if list_protocol != None and list_protocol != protocol and protocol not in RuleList.protocols:
+			return 0
+		return int(list_ip == ip) + int(list_port == port) + int(list_protocol == protocol) + 1
+	def getScore(self, l, ip, port, protocol):
+		score = 0
+		for rule in l:
+			cur = self.getSingleScore(rule[0], rule[1], rule[2], ip, port, protocol)
+			if cur > score:
+				score = cur
+		return score
 	def query(self, ip, port, protocol):
-		return True
+		white_score = self.getScore(self.white_list, ip, port, protocol)
+		black_score = self.getScore(self.black_list, ip, port, protocol)
+		if white_score <= black_score:
+			return 0
+		else:
+			return 1
+	def isEmpty(self):
+		return len(self.white_list) == 0 and len(self.black_list) == 0
 
 class PacketData:
 	macParser = MacParser("oui.csv")
@@ -49,22 +76,33 @@ class PacketData:
 		self.allowed = allowed
 		self.source_org = PacketData.macParser.parse(self.source_mac)
 
-def CSV2List(filename):
+def CSV2ListAndDict(filename):
 	'''
 	Return things like:
 	{'date': datetime.date(2018, 4, 7), 'time': datetime.time(8, 55, 43), 'source_mac': ['AA', 'BB', 'CC', 'DD', 'EE', 'FF'], 'source_ip': ['11', '22', '33', '44'], 'source_port': 1234, 'dest_mac': ['11', '22', '33', '44', '55', '66'], 'dest_ip': ['55', '66', '77', '88'], 'dest_port': 5678, 'protocol': 'HTTP', 'good_packet': 1, 'allowed': 1}
 	'''
 	train_set = np.loadtxt(filename, skiprows=1, delimiter=",", dtype="str")
 	train_set.reshape((-1, 11))
-	data = []
+	datas = []
 	for line in train_set:
 		packetData = PacketData(parser.parse(line[0]).date(), parser.parse(line[1]).time(), \
-			line[2], line[3], int(line[4]),\
-			line[5], line[6], int(line[7]),\
+			line[2], line[3], int(line[4]) if len(line[4]) != 0 else -1,\
+			line[5], line[6], int(line[7]) if len(line[7]) != 0 else -1,\
 			line[8], int(line[9]), int(line[10])\
 			)
-		data.append(packetData)
-	return data
+		datas.append(packetData)
+	d = {}
+	for data in datas:
+		d[data.source_mac] = d.get(data.source_mac, RuleList())
+	return datas, d
+
+def List2CSV(datas, filename):
+	f = open(filename, "w")
+	for data in datas:
+		f.write("%s, %s, %s, %s, %s, %s, %s, %s, %d, %d\n" % (data.date.strftime("%Y/%m/%d"), data.time.strftime("%H:%M:%S"), \
+			data.source_mac, data.source_ip, str(data.source_port) if data.source_port >= 0 else "", \
+			data.dest_ip, str(data.dest_port) if data.dest_port >= 0 else "", data.protocol, \
+			data.good_packet, data.allowed))
 
 def List2JSON(datas):
 	groups = {}
@@ -84,16 +122,49 @@ def List2JSON(datas):
 				{\
 					"id": [index], \
 					"date": data.date.strftime("%Y/%m/%d"), \
-					"time": data.time.strftime("%H/%M/%S"), \
+					"time": data.time.strftime("%H:%M:%S"), \
 					"source_ip": data.source_ip, \
-					"source_port": data.source_port, \
+					"source_port": str(data.source_port) if data.source_port >= 0 else "", \
 					"dest_mac": data.dest_mac, \
 					"dest_ip": data.dest_ip, \
-					"dest_port": data.dest_port, \
+					"dest_port": str(data.dest_port) if data.dest_port >= 0 else "", \
 					"protocol": data.protocol, \
 					"good_packet": data.good_packet,
 					"allowed": data.allowed
 				})
 	return json.dumps(groups)
 
+def tagList(datas, rules):
+	for data in datas:
+		if len(data.dest_ip) == 0:
+			data.good_packet = 1
+			data.allowed = 1
+			continue
+		if IP(data.dest_ip).iptype() == 'PUBLIC':
+			rl = rules[data.source_mac]
+			if rl.isEmpty():
+				rl.white_list.append((data.dest_ip, None, None))
+				data.good_packet = 1
+				data.allowed = 1
+			else:
+				data.good_packet = rl.query(data.dest_ip, data.dest_port, data.protocol)
+				data.allowed = data.good_packet
+		else:
+			data.good_packet = 1
+			data.allowed = 1
+
 # datas: [PacketData] rules: {"11:22:33:11:22:33": ruleList}
+'''
+from data import CSV2ListAndDict
+
+datas, rules = CSV2ListAndDict("train.csv")
+for data in datas:
+	do something
+	rl = ruleList[data.source_mac]
+	rl.white_list.append()
+	rl.black_list.append()
+	rl.query()
+'''
+'''
+http://localhost:8080/api/post
+'''
